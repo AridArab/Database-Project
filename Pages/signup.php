5d74d2e090db12075c3c927d2445321e2fb2b412
Xavier Gaviola <rickygaviola@outlook.com>
2023-03-20 14:41:47 -0500

    Updated References to index.css

--- Pages/signup.php
+++ Pages/signup.php
@@ -1,12 +1,12 @@
 <?php
-    $name = '';
-    $nameErr = '';
+$name = '';
+$nameErr = '';
 ?>
 
 <!DOCTYPE html>
 <html>
 <header>
-    <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="../index.css">
     <meta charset="UTF-8">
     <meata http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- Pages/view_Team.php
+++ Pages/view_Team.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <header>
-    <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="../index.css">
     <meta charset="UTF-8">
     <meata http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
