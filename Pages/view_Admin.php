c1938aceec4ab810b03145bbd8d856bfbc0c142a
Zohair Alsheikh <zohairalsheikh2002@gmail.com>
2023-04-20 13:53:36 -0500

    Added display for all departments in database

--- Pages/view_Admin.php
+++ Pages/view_Admin.php
@@ -9,19 +9,51 @@
         echo '</script>';
         exit();
     }
+
+    $conn = connect();
+
+    $result = sqlsrv_query($conn,
+        "SELECT * FROM Department"
+    );
+
 ?>
 
 <html>
 
 <header>
     <style>
-
+        .department {
+            border: 1px solid rgb(0, 0, 0);
+            text-align: left;
+            padding-left: 25px;
+            padding-right: 25px;
+            padding-top: 40px;
+            padding-bottom: 40px;
+            margin-top: 10px;
+            margin-bottom: 10px;
+            width: 65%;
+            background-color: rgba(225, 225, 225, 0.75);
+            border-radius: 25px;            
+        }
     </style>
 </header>
 
 <body>
     <center>
         <h1>Admin</h1>
+        <p><?php
+        //Unsure how to retrieve first and last name from query -Zohair
+         while($row=sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)){
+            $manager = select_query("SELECT Employee.First_Name, Employee.Last_Name FROM Department, Employee WHERE Employee.ID = Department.Manager_ID", $conn);
+            echo
+            "<div class='department'>
+            $row[Dept_Name]<br>
+            Email: $row[Email_Address]<br>
+            Phone: $row[Phone_Number]<br>
+            Manager: $manager[1]
+            </div>";
+         }
+        ?></p>
     </center>
 </body>
 
