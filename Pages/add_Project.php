ec15173396269b2bda785fdd8a5a1d09987749c1
DoneshD <donesh.dava@gmail.com>
2023-04-22 20:32:50 +0000

    add fix

--- Pages/add_Project.php
+++ Pages/add_Project.php
@@ -7,10 +7,8 @@ if($conn === false ) {
      die( print_r( sqlsrv_errors(), true));
 }
 
-
-$sql = "INSERT INTO Project (Progress, Name, Total_Cost, Street_Address, City, State, Zip_Code, Department_ID, Budget, isActive, Start_Date, End_Date, Deadline) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-$params = array($_POST['progress'], $_POST['projectName'], $_POST['cost'], 
-$_POST['address'], $_POST['city'], $_POST['state'], $_POST['zip'], $_POST['deptID'], $_POST['budget'], 1, $_POST['startdate'], NULL, $_POST['deadline']);
+$sql =  "INSERT INTO Project (Progress, Name, Total_Cost, Street_Address, City, State, Zip_Code, Department_ID, Budget, isActive, Start_Date, End_Date, Deadline) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$params = array($_POST['progress'], $_POST['projectName'], $_POST['cost'], $_POST['address'], $_POST['city'], $_POST['state'], $_POST['zip'], $_POST['deptID'], $_POST['budget'], 1, $_POST['startdate'], NULL, $_POST['deadline']);
 
 $stmt = sqlsrv_query( $conn, $sql, $params);
 if( $stmt === false ) {
